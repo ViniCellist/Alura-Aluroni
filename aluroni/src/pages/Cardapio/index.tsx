9210ab2f198bccddbf036e2aa2e24b41450a540b
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-07 16:38:22 -0300

    finished

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -35,7 +35,11 @@ export default function Cardapio() {
                         setOrdenador={setOrdenador}
                     />
                 </div>
-                <Itens />
+                <Itens 
+                    busca={busca} 
+                    filtro={filtro}
+                    ordenador={ordenador}
+                />
             </section>
         </main>
     )
