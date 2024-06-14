166a22611d55fccf01eeca251874cc3ab8dc851e
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-14 20:31:31 -0300

    feat footer and notfound page

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -1,7 +1,10 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Sobre from 'pages/Sobre';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function AppRouter() {
@@ -13,8 +16,11 @@ export default function AppRouter() {
                     <Route path='/' element={<PaginaPadrao />} >
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
+                        <Route path='sobre' element={<Sobre />} />
+                        <Route path='*' element={<NotFound />} />
                     </Route>
                 </Routes>
+                <Footer />
             </Router>
         </main>
     )
