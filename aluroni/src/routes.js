d87b019c8e42ebf9055483e476250470387acec0
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-14 20:55:21 -0300

    update

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -4,24 +4,26 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
-    return (
-        <main>
-            <Router>
-                <Menu />
-                <Routes>
-                    <Route path='/' element={<PaginaPadrao />} >
-                        <Route index element={<Inicio />} />
-                        <Route path='cardapio' element={<Cardapio />} />
-                        <Route path='sobre' element={<Sobre />} />
-                        <Route path='*' element={<NotFound />} />
-                    </Route>
-                </Routes>
-                <Footer />
-            </Router>
-        </main>
-    )
+  return (
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />} >
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+            <Route path='prato/:id' element={<Prato />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+  );
 }
