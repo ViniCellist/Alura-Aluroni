31b85e5b45918d0ce9f73ca9f92b3c644b4dbdca
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-13 22:12:22 -0300

    update

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from 'assets/logo.svg';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Items from './Itens/index';
@@ -14,9 +13,6 @@ export default function Cardapio() {
     const [ordenador, setOrdenador] = useState("");
     return (
         <main>
-            <nav className={styles.menu}>
-                <Logo />
-            </nav>
             <header className={styles.header}>
                 <div className={styles.header__text}>
                     A casa do código e da massa
